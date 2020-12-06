b79aa6df34d4a2acfa38e3cc5b1c9b3a4181c710
devkevkeenan <44340382+devkevkeenan@users.noreply.github.com>
2020-12-06 13:06:59 +0000

    Fix tests from previous day

--- AdventOfCode.Tests/MethodTests/GetNumberOfValidPasswords.cs
+++ AdventOfCode.Tests/MethodTests/GetNumberOfValidPasswords.cs
@@ -10,45 +10,41 @@ namespace AdventOfCode.Tests.MethodTests
         {
         }
 
-        private static IEnumerable<TestCaseData> GetPasswordTests
+        private static IEnumerable<TestCaseData> GetPasswordTests_PositionFalse
         {
             get
             {
                 yield return new TestCaseData(
                         arg: new string[] {"1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"})
-                    .SetName("Example")
-                    .Returns(1);
+                    .Returns(2);
                 yield return new TestCaseData(
                         arg: new string[] { "1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc", "2-9 c: ccccccccc" })
-                    .SetName("Duplicate")
-                    .Returns(1);
+                    .Returns(3);
             }
         }
 
-        [Test, TestCaseSource(nameof(GetPasswordTests))]
-        public int GetNumberOfValidPasswordsWithoutPosition(string[] list)
+        [Test, TestCaseSource(nameof(GetPasswordTests_PositionFalse), Category = "x")]
+        public int GetNumberOfValidPasswords_PositionFalse(string[] list)
         {
             return Methods.GetNumberOfValidPasswords(list, false);
         }
 
 
-        private static IEnumerable<TestCaseData> GetPasswordTests2
+        private static IEnumerable<TestCaseData> GetPasswordTests_PositionTrue
         {
             get
             {
                 yield return new TestCaseData(
                         arg: new string[] { "1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc" })
-                    .SetName("Example")
                     .Returns(1);
                 yield return new TestCaseData(
                         arg: new string[] { "1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc", "2-9 c: ccccccccc" })
-                    .SetName("Duplicate")
-                    .Returns(2);
+                    .Returns(1);
             }
         }
 
-        [Test, TestCaseSource(nameof(GetPasswordTests2))]
-        public int GetNumberOfValidPasswordsWithPosition(string[] list)
+        [Test, TestCaseSource(nameof(GetPasswordTests_PositionTrue), Category = "y")]
+        public int GetNumberOfValidPasswords_PositionTrue(string[] list)
         {
             return Methods.GetNumberOfValidPasswords(list, true);
         }
